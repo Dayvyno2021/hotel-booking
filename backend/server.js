a259596536e16113bca1daf4027f4d13613b0349
Dayvyno2021 <dayvynomern2021@gmail.com>
2022-01-29 22:08:20 +0100

    resolving deployment issue

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ connectDB()
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(morgan('dev'))
+
+if (process.env.NODE_ENV==="development"){
+  app.use(morgan('dev'))
+}
 
 
 
